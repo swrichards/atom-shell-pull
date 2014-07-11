bff27aff4e577686cb9a6e7015d8b387605b31a4
Vasari <nmccallum7@gmail.com>
2014-07-11 21:59:41 +1000

    Starting version. Gets the urls to download of the specified platform.

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-console.log('Hello, World!');
+var path = require('path');
+
+var github = require('octonode');
+var _ = require('lodash');
+
+module.exports = function (options) {
+    var self = this;
+
+    self.version = options.version;
+    self.platforms = options.platforms;
+    self.outputDir = path.join(process.cwd(), options.outputDir);
+    self.toDownload = [];
+
+    self.prepare = function (readyCallback) {
+        // Get an array of files to download.
+        console.log('Getting urls to release binaries.');
+        var client = github.client();
+        var repo = client.repo('atom/atom-shell');
+        var releases = repo.releases(function (error, data, headers) {
+            var latest = _.first(_.take(data, function (release) {
+                // Ignore drafts and pre-releases.
+                return !release.draft && !release.prerelease;
+            }));
+
+            self.toDownload = self.getUrlsForPlatforms(latest.assets);
+            readyCallback.call(self);
+        });
+    },
+
+    self.getUrlsForPlatforms = function (assets) {
+        return _.chain(assets)
+            .pluck('browser_download_url')
+
+            // 32 bit should work on 32 and 64. If you want 64 bit, send an issue. :)
+            // Also remove debugging symbols from the list.
+            .filter(function (url) {
+                var foo = url.match(/ia32/);
+                var debug = url.match(/symbols/);
+                var matchesPlatform = false;
+
+                _.each(self.platforms, function (platform) {
+                    if (url.match(new RegExp(platform))) {
+                        matchesPlatform = true;
+                        return false;
+                    }
+                });
+
+                return foo && !debug && matchesPlatform;
+            })
+            .value();
+    };
+
+    self.start = function () {
+        if (self.toDownload.length === 0) {
+            console.log('Nothing to download!');
+            return;
+        }
+
+        // Start downloading files.
+        console.log('Starting to download for platforms: ' + self.platforms.join(', '));
+
+        var downloaded = 0;
+        _.each(self.toDownload, function (url) {
+            console.log('Downloading ' + _.last(url.split('/')))
+
+            // TODO!
+
+            downloaded += 1;
+        });
+
+        console.log('Finished downloading ' + downloaded + ' binaries.');
+    };
+
+}
--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("We're good to go, man!");
+var ShellDownload = require('./index');
+
+var path = require('path');
+
+var output = path.join(__dirname, 'downloads');
+
+var download = new ShellDownload({
+    outputDir : output,
+    platforms : ['linux'] // Available options are 'linux', 'win32' and 'darwin'
+});
+
+download.prepare(function () {
+    this.start();
+});
