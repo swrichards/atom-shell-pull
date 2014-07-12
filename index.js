dc54cbdcbce3d0c02757b9a67e195a0f46f31524
Vasari <nmccallum7@gmail.com>
2014-07-12 10:17:31 +1000

    It kinda downloads stuff. :+1:

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var path = require('path');
 
 var github = require('octonode');
 var _ = require('lodash');
+var spawn = require('child_process').spawn;
 
 module.exports = function (options) {
     var self = this;
 
     self.version = options.version;
     self.platforms = options.platforms;
-    self.outputDir = path.join(process.cwd(), options.outputDir);
+    self.outputDir = path.resolve(options.outputDir);
     self.toDownload = [];
 
     self.prepare = function (readyCallback) {
@@ -31,11 +32,15 @@ module.exports = function (options) {
         return _.chain(assets)
             .pluck('browser_download_url')
 
-            // 32 bit should work on 32 and 64. If you want 64 bit, send an issue. :)
-            // Also remove debugging symbols from the list.
             .filter(function (url) {
+
+                // 32 bit should work on 32 and 64. If you want 64 bit, send an issue. :)
                 var foo = url.match(/ia32/);
+
+                // Also remove debugging symbols from the list.
                 var debug = url.match(/symbols/);
+
+                // Finally sort out the platform stuff.
                 var matchesPlatform = false;
 
                 _.each(self.platforms, function (platform) {
@@ -59,16 +64,30 @@ module.exports = function (options) {
         // Start downloading files.
         console.log('Starting to download for platforms: ' + self.platforms.join(', '));
 
-        var downloaded = 0;
-        _.each(self.toDownload, function (url) {
-            console.log('Downloading ' + _.last(url.split('/')))
+        // var download = wget.download(_.first(self.toDownload), self.outputDir, {});
+        // download.on('error', function (error) {
+        //     console.log(error);
+        // });
 
+        // download.on('end', function (output) {
+        //     console.log(output);
+        // });
+
+        // download.on('progress', function (progress) {
+        //     console.log(progress);
+        // });
+
+        // This is unsafe - yolo!
+        var download = spawn('wget', [_.first(self.toDownload), '-P', self.outputDir]);
+        // For some reason all the status updates are sent to stderr.
+        download.stderr.on('data', function (data) {
+            // Find the percent values and put 'em into our progress bar.
             // TODO!
-
-            downloaded += 1;
+            // .. 37%
         });
-
-        console.log('Finished downloading ' + downloaded + ' binaries.');
+        download.on('close', function (code) {
+            console.log('Download ended with code ' + code);
+        });
     };
 
 }
